3de3e15302f93a6de187b749e70c822969eb09d3
lahiruharshana <lharshana2002@>
2023-07-29 13:14:04 +0530

    0.1.5

--- src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
+++ src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
@@ -2,6 +2,8 @@ package lk.ijse.gdse.orm.hibernate.config;
 
 public class SessionFactoryConfig {
 
+    private static SessionFactoryConfig factoryConfig;
+
     private SessionFactoryConfig(){
 
     }
