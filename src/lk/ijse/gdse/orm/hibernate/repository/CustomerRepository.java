82d8362a29ba4ec87e0215bb8e8e144c8c4b9278
lahiruharshana <lharshana2002@>
2023-08-05 15:20:31 +0530

    0.4.1

--- src/lk/ijse/gdse/orm/hibernate/repository/CustomerRepository.java
+++ src/lk/ijse/gdse/orm/hibernate/repository/CustomerRepository.java
@@ -13,7 +13,12 @@ public class CustomerRepository {
 
     }
     public Customer getCustomer(int id){
-        return session.get(Customer.class,id);
+        try {
+            return session.get(Customer.class,id);
+        }catch (Exception e){
+            e.printStackTrace();
+            throw e;
+        }
     }
 
     public int saveCustomer(Customer customer){
@@ -32,15 +37,31 @@ public class CustomerRepository {
     }
     public boolean updateCustomer(Customer customer){
         Transaction transaction = session.beginTransaction();
-        session.update(customer);
-        return true;
+        try {
+            session.update(customer);
+            return true;
+        }catch (Exception e){
+            transaction.rollback();
+            session.close();
+            e.printStackTrace();
+            return false;
+        }
+
     }
 
     public boolean deleteCustomer(Customer customer){
         Transaction transaction = session.beginTransaction();
-        session.delete(customer);
-        transaction.commit();
-        session.close();
-        return true;
+        try{
+            session.delete(customer);
+            transaction.commit();
+            session.close();
+            return true;
+
+        }catch (Exception e){
+            transaction.rollback();
+            session.close();
+            e.printStackTrace();
+            return false;
+        }
     }
 }
